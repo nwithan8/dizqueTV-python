3674cfae48d372b5897344713e2931e7c08960e0
Nate <nathan.harris@force5solutions.com>
2020-08-29 21:51:08 -0400

    Moved logging back to logging.py rather than requests.py, for easier module import when logging needed in the future

--- dizqueTV/logging.py
+++ dizqueTV/logging.py
@@ -1,4 +1,31 @@
 import logging
 
-logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s',
-                    level=logging.INFO)
+
+def info(message):
+    logging.info(msg=message)
+
+
+def error(message):
+    logging.error(msg=message)
+
+
+def warning(message):
+    logging.warning(msg=message)
+
+
+level_map = {
+    'info': info,
+    'error': error,
+    'warning': warning
+}
+
+
+def log(message: str, level: str = "info") -> None:
+    """
+    Log a message if verbose is enabled.
+    :param message: Message to log
+    :param level: info, error or warning
+    """
+    if level not in level_map.keys():
+        level = 'info'
+    level_map[level](message)
--- dizqueTV/requests.py
+++ dizqueTV/requests.py
@@ -1,30 +1,20 @@
 from urllib.parse import urlencode
 from typing import Union
-from logging import info, error, warning
 
 import requests
 
-import dizqueTV.logging
-
-
-def _log(message: str, level: Union[info, error, warning] = info) -> None:
-    """
-    Log a message if verbose is enabled.
-    :param message: Message to log
-    :param level: info, error or warning
-    """
-    level(message)
+import dizqueTV.logging as logs
 
 
 def get(url: str,
         params: dict = None,
         headers: dict = None,
         timeout: int = 2,
-        log: Union[info, error, warning] = None) -> Union[requests.Response, None]:
+        log: str = None) -> Union[requests.Response, None]:
     if params:
         url += f"?{urlencode(params)}"
     if log:
-        _log(message=f"GET {url}", level=log)
+        logs.log(message=f"GET {url}", level=log)
     try:
         return requests.get(url=url, headers=headers, timeout=timeout)
     except requests.exceptions.Timeout:
@@ -36,11 +26,11 @@ def post(url: str,
          headers: dict = None,
          data: dict = None,
          timeout: int = 2,
-         log: Union[info, error, warning] = None) -> Union[requests.Response, None]:
+         log: str = None) -> Union[requests.Response, None]:
     if params:
         url += f"?{urlencode(params)}"
     if log:
-        _log(message=f"POST {url}, Body: {data}", level=info)
+        logs.log(message=f"POST {url}, Body: {data}", level=log)
     try:
         return requests.post(url=url, json=data, headers=headers, timeout=timeout)
         # use json= rather than data= to convert single-quoted dict to double-quoted JSON
@@ -53,11 +43,11 @@ def put(url: str,
         headers: dict = None,
         data: dict = None,
         timeout: int = 2,
-        log: Union[info, error, warning] = None) -> Union[requests.Response, None]:
+        log: str = None) -> Union[requests.Response, None]:
     if params:
         url += f"?{urlencode(params)}"
     if log:
-        _log(message=f"PUT {url}, Body: {data}", level=info)
+        logs.log(message=f"PUT {url}, Body: {data}", level=log)
     try:
         return requests.put(url=url, json=data, headers=headers, timeout=timeout)
         # use json= rather than data= to convert single-quoted dict to double-quoted JSON
@@ -70,11 +60,11 @@ def delete(url: str,
            headers: dict = None,
            data: dict = None,
            timeout: int = 2,
-           log: Union[info, error, warning] = None) -> Union[requests.Response, None]:
+           log: str = None) -> Union[requests.Response, None]:
     if params:
         url += f"?{urlencode(params)}"
     if log:
-        _log(message=f"DELETE {url}", level=info)
+        logs.log(message=f"DELETE {url}", level=log)
     try:
         return requests.delete(url=url, json=data, headers=headers, timeout=timeout)
         # use json= rather than data= to convert single-quoted dict to double-quoted JSON
