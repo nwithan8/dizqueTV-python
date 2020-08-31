e70e83f5a3c408174a035254c24f9dfaab12d6ee
Nate <nathan.harris@force5solutions.com>
2020-08-30 21:40:54 -0400

    PyPi required files

---
+++ dizqueTV/__init__.py
@@ -0,0 +1,1 @@
+from dizqueTV.dizquetv import API, convert_plex_item_to_filler, convert_plex_item_to_program, convert_plex_server_to_dizque_plex_server
---
+++ setup.py
@@ -0,0 +1,53 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='dizqueTV',  # How you named your package folder (MyLib)
+    packages=['dizqueTV'],  # Chose the same as "name"
+    version='0.0.64',  # Start with a small number and increase it with every change you make
+    license='GNU General Public License v3 (GPLv3)',
+    description="Interact with a dizqueTV instance's API",  # Give a short description about your library
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Nate Harris',  # Type in your name
+    author_email='n8gr8gbln@gmail.com',  # Type in your E-Mail
+    url='https://github.com/nwithan8/dizqueTV-python',  # Provide either the link to your github or to your website
+    download_url='https://github.com/nwithan8/dizqueTV-python/archive/0.0.64.tar.gz',  # I explain this later on
+    keywords=[
+        'dizqueTV',
+        'Plex',
+        'Jellyfin',
+        'Emby',
+        'media',
+        'API',
+        'server',
+        'interaction',
+        'TV',
+        'television',
+        'streaming'
+    ],  # Keywords that define your package best
+    install_requires=[
+        'requests',
+        'm3u8',
+        'plexapi'
+    ],
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Development Status :: 3 - Alpha',
+        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Intended Audience :: Developers',  # Define that your audience are developers
+        'Topic :: Software Development :: Build Tools',
+        'Programming Language :: Python :: 3',  # Specify which python versions that you want to support
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Multimedia :: Video',
+        'Topic :: Multimedia',
+        'Topic :: Internet :: WWW/HTTP',
+        'Operating System :: OS Independent'
+    ],
+    python_requires='>=3.6'
+)
