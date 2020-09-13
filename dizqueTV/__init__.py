d36b38106f7239b441ec5daacf64a9349768b6d8
Nate <nathan.harris@force5solutions.com>
2020-09-13 02:16:18 -0400

    Moved version and author to _version.py

--- dizqueTV/__init__.py
+++ dizqueTV/__init__.py
@@ -1,6 +1,3 @@
 from dizqueTV.dizquetv import API, convert_plex_item_to_filler, convert_plex_item_to_program, \
     convert_plex_server_to_dizque_plex_server
-
-
-__version__ = '0.0.64'
-__author__ = 'Nate Harris'
+from ._version import __author__, __version__
