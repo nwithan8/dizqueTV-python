33194968f7371c40e7cb5ec6aca7a82d00ed8064
Nate <nathan.harris@force5solutions.com>
2020-11-28 14:09:39 -0700

    Changes to package info

--- dizqueTV/__init__.py
+++ dizqueTV/__init__.py
@@ -1,3 +1,3 @@
 from dizqueTV.dizquetv import API, convert_plex_item_to_filler_item, convert_plex_item_to_program, \
     convert_plex_server_to_dizque_plex_server, make_time_slot_from_dizque_program
-from ._info import __author__, __version__, __license__, __copyright__, __author_email__
+from ._version import __author__, __version__, __license__, __copyright__, __author_email__
