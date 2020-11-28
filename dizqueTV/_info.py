2f36dae16d019bd8467dff46a39ab52a5793f490
Nate <nathan.harris@force5solutions.com>
2020-11-28 14:09:39 -0700

    Changes to package info

--- dizqueTV/_info.py
+++ dizqueTV/_info.py
@@ -1,5 +1,6 @@
-__title__ = "dizqueTV"
 __version__ = '1.2.2.1'
+
+__title__ = "dizqueTV"
 __author__ = 'Nate Harris'
 __author_email__ = 'n8gr8gbln@gmail.com'
 __copyright__ = "Copyright © 2020 - Nate Harris"
