432a3312b9a8e4c190beed1dbf4c432c55a79c2d
Nate Harris <n8gr8gbln@gmail.com>
2021-01-02 00:37:29 -0700

    Icon for episodes is now show poster, not episode thumbnail

--- dizqueTV/helpers.py
+++ dizqueTV/helpers.py
@@ -230,6 +230,7 @@ def _make_program_dict_from_plex_item(plex_item: Union[Video, Movie, Episode], p
         data['episodeIcon'] = f"{plex_uri}{plex_item.thumb}?X-Plex-Token={plex_token}"
         data['seasonIcon'] = f"{plex_uri}{plex_item.parentThumb}?X-Plex-Token={plex_token}"
         data['showIcon'] = f"{plex_uri}{plex_item.grandparentThumb}?X-Plex-Token={plex_token}"
+        data['icon'] = data['showIcon']
     return data
 
 
