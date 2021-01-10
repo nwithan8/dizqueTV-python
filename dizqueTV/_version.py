6b6f67326492590b98364b686c25ada9322729d7
Nate <nathan.harris@force5solutions.com>
2021-01-10 16:40:33 -0700

    Use _info rather than _version.py

--- dizqueTV/_version.py
+++
@@ -1,6 +0,0 @@
-__title__ = "dizqueTV"
-__version__ = '1.2.2.1'
-__author__ = 'Nate Harris'
-__author_email__ = 'n8gr8gbln@gmail.com'
-__copyright__ = "Copyright © 2020 - Nate Harris"
-__license__ = 'GNU General Public License v3 (GPLv3)'
--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import setuptools
-import dizqueTV._version as package_info
+import dizqueTV._info as package_info
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
