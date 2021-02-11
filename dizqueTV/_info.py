c2738524dc2e7cc130ac632faa13290782b84cf0
Nate <nathan.harris@force5solutions.com>
2021-02-11 00:58:02 -0700

    Updated copyright

--- dizqueTV/_info.py
+++ dizqueTV/_info.py
@@ -3,7 +3,7 @@ __version__ = '1.3.0.0'
 __title__ = "dizqueTV"
 __author__ = 'Nate Harris'
 __author_email__ = 'n8gr8gbln@gmail.com'
-__copyright__ = "Copyright © 2020 - Nate Harris"
+__copyright__ = "Copyright © 2021 - Nate Harris"
 __license__ = 'GNU General Public License v3 (GPLv3)'
 __docs__ = "https://dizquetv.readthedocs.io/en/latest"
 
