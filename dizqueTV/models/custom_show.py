430b4aa7f1f2daddf62e505eb9053bef9544c388
Nate <nathan.harris@force5solutions.com>
2021-03-24 17:34:33 -0600

    Models for CustomShows et. al

--- dizqueTV/models/custom_show.py
+++ dizqueTV/models/custom_show.py
@@ -1,13 +1,63 @@
-from dizqueTV.models.base import BaseObject, BaseAPIObject
+from typing import Union, List
 
-class CustomShowItem(BaseObject):
-    def __init__(self, data: dict):
-        super().__init__(data)
+from dizqueTV.models.media import Program
 
-class CustomShowDetails(BaseObject):
-    def __init__(self, data: dict):
-        super().__init__(data)
+from dizqueTV.models.base import BaseAPIObject
+
+
+class CustomShowItem(Program):
+    def __init__(self, data: dict, dizque_instance):
+        super().__init__(data, dizque_instance, None)
+        self._full_data = data
+        self.durationStr = data.get('durationStr')
+        self._commercials = []
+
+    @property
+    def _data(self):
+        """
+        Override default self._data to ignore durationStr and commercials
+
+        :return: Data dict
+        :rtype: dict
+        """
+        data = self._full_data
+        data.pop('durationStr', None)
+        data.pop('commercials', None)
+        return data
+
+    @property
+    def commercials(self) -> List:
+        """
+        Get the show's commercials
+
+        :return: List of commercials
+        :rtype: list
+        """
+        if not self._commercials:
+            self._commercials = []
+        return self._commercials
+
+
+class CustomShowDetails(BaseAPIObject):
+    def __init__(self, data: dict, dizque_instance):
+        super().__init__(data, dizque_instance)
         self.name = data.get('name')
+        self.id = data.get('id')
+        self._content = []
+
+    @property
+    def content(self) -> List[CustomShowItem]:
+        """
+        Get the custom show's content (the actual programs)
+
+        :return: list of CustomShowItem objects
+        :rtype: list
+        """
+        if not self._content:
+            self._content = [CustomShowItem(item_data, self._dizque_instance) for item_data in
+                             self._data.get('content', [])]
+        return self._content
+
 
 class CustomShow(BaseAPIObject):
     def __init__(self, data: dict, dizque_instance):
@@ -15,12 +65,30 @@ class CustomShow(BaseAPIObject):
         self.id = data.get('id')
         self.name = data.get('name')
         self.count = data.get('count')
+        self.customShowTag = "customShow"
         self._details = None
 
     @property
-    def details(self):
+    def details(self) -> Union[CustomShowDetails, None]:
+        """
+        Get the custom show's details
+
+        :return: CustomShowDetails object
+        :rtype: CustomShowDetails
+        """
         if not self._details:
             self._details = self._dizque_instance.get_custom_show_details(custom_show_id=self.id)
         return self._details
 
+    @property
+    def content(self):
+        """
+        Get the custom show's content (the actual programs)
 
+        :return: list of CustomShowItem objects
+        :rtype: list
+        """
+        details = self.details
+        if not details:
+            return []
+        return details.content
