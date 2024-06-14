bbd57d65de10b48745697894ad1a1f2e8a6c921d
Nate Harris <nwithan8@users.noreply.github.com>
2024-06-14 11:24:11 -0600

    - Update release process

--- setup.py
+++ setup.py
@@ -1,49 +1,104 @@
+import json
+import os
+from typing import List
+
 import setuptools
-import dizqueTV._info as package_info
+
+__version__ = 'VERSIONADDEDBYGITHUB'
+
+__title__ = "dizqueTV"
+__author__ = 'Nate Harris'
+__author_email__ = 'n8gr8gbln@gmail.com'
+__github_username__ = "nwithan8"
+__github_repo__ = "dizqueTV-python"
+__copyright__ = "Copyright © YEARADDEDBYGITHUB - Nate Harris"
+__license__ = 'GNU General Public License v3 (GPLv3)'
+__docs__ = "https://dizquetv.readthedocs.io/en/latest"
+__description__ = "Interact with a dizqueTV instance's API"
+__keywords__ = [
+    "dizqueTV",
+    "Plex",
+    "Jellyfin",
+    "Emby",
+    "media",
+    "API",
+    "server",
+    "interaction",
+    "TV",
+    "television",
+    "streaming",
+]
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-with open("requirements.txt", 'r') as fh:
-    requirements = fh.read().splitlines()
+with open("requirements.txt", "r") as fh:
+    REQUIREMENTS = fh.read().splitlines()
 
-with open("dev_requirements.txt", 'r') as fh:
-    dev_requirements = fh.read().splitlines()
+with open("dev_requirements.txt", "r") as fh:
+    DEV_REQUIREMENTS = fh.read().splitlines()
+
+
+def __supported_python_versions__() -> List[str]:
+    """Return a list of supported Python versions."""
+    with open(os.path.join(os.path.dirname(__file__), "dizqueTV", "PYTHON_VERSIONS.json")) as f:
+        versions = f.read()
+        return json.loads(versions)
+
+
+def python_versions() -> List[str]:
+    """Return a list of supported Python versions."""
+    versions = __supported_python_versions__()
+    version_strings = ['Programming Language :: Python :: 3']
+    for version in versions:
+        version_strings.append(f"Programming Language :: Python :: {version}")
+    return version_strings
+
+
+def python3_range() -> str:
+    """Return a string of the supported Python version range."""
+    versions = __supported_python_versions__()
+    return f">={versions[0]}, <4"
+
+
+classifiers = [
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    'Development Status :: 4 - Beta',
+    # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',  # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'Topic :: Multimedia :: Video',
+    'Topic :: Multimedia',
+    'Topic :: Internet :: WWW/HTTP',
+    'Operating System :: OS Independent'
+]
+classifiers.extend(python_versions())
+
+# Replace version if not replaced already
+try:
+    _ = setuptools._vendor.packaging.version.Version(__version__)
+except Exception:
+    __version__ = '0.0.0'
 
 setuptools.setup(
-    name=package_info.__title__,  # How you named your package folder (MyLib)
-    packages=setuptools.find_packages(),  # Choose the same as "name"
-    version=package_info.__version__,  # Start with a small number and increase it with every change you make
-    license=package_info.__license__,
-    description=package_info.__description__,  # Give a short description about your library
+    name=__title__,
+    packages=setuptools.find_packages(exclude=["tests"]),
+    include_package_data=True,
+    version=__version__,
+    license=__license__,
+    description=__description__,
     long_description=long_description,
     long_description_content_type="text/markdown",
-    author=package_info.__author__,  # Type in your name
-    author_email=package_info.__author_email__,  # Type in your E-Mail
-    url='https://github.com/nwithan8/dizqueTV-python',  # Provide either the link to your github or to your website
-    download_url=f'https://github.com/nwithan8/dizqueTV-python/archive/refs/tags/{package_info.__version__}.tar.gz',
-    keywords=package_info.__keywords__,  # Keywords that define your package best
-    install_requires=requirements,
+    author=__author__,
+    author_email=__author_email__,
+    url=f'https://github.com/{__github_username__}/{__github_repo__}',
+    download_url=f'https://github.com/{__github_username__}/{__github_repo__}/archive/refs/tags/{__version__}.tar.gz',
+    keywords=__keywords__,
+    install_requires=REQUIREMENTS,
     extras_require={
-        "dev": dev_requirements,
+        "dev": DEV_REQUIREMENTS,
     },
-    classifiers=[
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Development Status :: 4 - Beta',
-        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-        'Intended Audience :: Developers',  # Define that your audience are developers
-        'Topic :: Software Development :: Build Tools',
-        'Programming Language :: Python :: 3',  # Specify which python versions that you want to support
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
-        'Topic :: Multimedia :: Video',
-        'Topic :: Multimedia',
-        'Topic :: Internet :: WWW/HTTP',
-        'Operating System :: OS Independent'
-    ],
-    python_requires='>=3.7'
+    test_suite="test",
+    classifiers=classifiers,
+    python_requires=python3_range(),
 )
